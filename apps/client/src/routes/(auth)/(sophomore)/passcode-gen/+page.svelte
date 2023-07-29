27444d9d54f5b3f361b4c80364c357c837813dd9
aaapwn <armmykung2547@gmail.com>
2023-07-29 22:02:37 +0700

    add genpasscode page

---
+++ apps/client/src/routes/(auth)/(sophomore)/passcode-gen/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+</script>
+
+<h1 class="text-center text-white font-Pridi font-thin text-4xl mt-1">รหัสลับของคุณคือ</h1>
+<div class="relative flex justify-center items-center mx-6 py-6 bg-[#29436C]/10 rounded-3xl border-[#29436C] drop-shadow-[0px_4px_4px_black] border-solid border mt-5">
+    <img src="./konnok.png" alt="" class=" absolute left-5 h-3/5 transform -scale-x-100" />
+    <h1 class="text-accent font-Pridi font-thin text-3xl">IPX-956</h1>
+    <img src="./konnok.png" alt="" class=" absolute right-5 h-3/5 transform" />
+</div>
+<div class="text-center font-Pridi text-sm text-accent font-extralight mt-10">
+    <p class="">เมื่อรหัสถูกใช้แล้วจะต้องรีเซ็ตใหม่ทุกรอบ</p>
+    
+</div>
