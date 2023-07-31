e75664614f25267bddc7c806dff2b75a894afd1b
DrowningToast <supratouchgussii@gmail.com>
2023-08-01 00:44:27 +0700

    chore

--- apps/client/src/lib/components/svelte/CardButtonMenu.svelte
+++ apps/client/src/lib/components/svelte/CardButtonMenu.svelte
@@ -1,21 +1,23 @@
 <script lang="ts">
-    export let isActived: boolean;
-    export let img_active: string;
-    export let img_inactive: string;
-    export let text: string;
-    export let link: string;
+	export let isActived: boolean;
+	export let img_active: string;
+	export let img_inactive: string;
+	export let text: string;
+	export let link: string;
 </script>
 
 <div class="flex flex-col justify-center items-center">
-    {#if isActived}
-        <a href={link}>
-            <img src={img_active} alt="">
-            <p class="text-center text-white text-[10px] mt-2 font-Pridi font-thin">{text}</p>
-        </a>
-    {:else}
-        <div>
-            <img src={img_inactive} alt="">
-            <p class="text-center text-white text-[10px] mt-2 opacity-10 font-Pridi font-thin">???????????????</p>
-        </div>
-    {/if}
+	{#if isActived}
+		<a href={link}>
+			<img src={img_active} alt="" />
+			<p class="text-center text-white text-[10px] mt-2 font-Pridi font-thin">{text}</p>
+		</a>
+	{:else}
+		<div>
+			<img src={img_inactive} alt="" />
+			<p class="text-center text-white text-[10px] mt-2 opacity-10 font-Pridi font-thin">
+				???????????????
+			</p>
+		</div>
+	{/if}
 </div>
