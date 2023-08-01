0a3af88e5ba93b8e8df394846708468d59112e3c
DrowningToast <supratouchgussii@gmail.com>
2023-08-02 00:34:41 +0700

    remove: unused

--- apps/client/src/routes/(auth)/+layout.server.ts
+++ apps/client/src/routes/(auth)/+layout.server.ts
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
-	const { session, user } = locals;
+	const { session } = locals;
 
 	// Check auth guard
 	if (!session) {
