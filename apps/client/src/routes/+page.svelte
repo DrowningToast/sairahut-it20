51a4d0580047649ffc65f555ac2a67162c34383a
aaapwn <armmykung2547@gmail.com>
2023-08-02 17:06:19 +0700

    add responsive

--- apps/client/src/routes/+page.svelte
+++ apps/client/src/routes/+page.svelte
@@ -7,7 +7,17 @@
 </script>
 
 <div class="bg-gradient-to-b from-primary to-black hidden md:block min-h-screen relative">
-	<h1 class="text-center text-accent font-extralight font-Pridi text-3xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">มึงจะไปเล่นในโทรศัพท์<br>หรือจะให้กูร่ายเวทใส่หัวพ่อมึง</h1>
+	<div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-accent-alt border border-accent p-5 rounded-xl">
+		<h1 class=" font-Pridi text-2xl font-medium">เรียน : ท่านผู้เดินทางไปยังโลกเวทย์มนต์</h1>
+		<div class="ml-3 mt-3 font-light">
+			<p class="indent-6">เนื่องด้วยประตูสู่โลกเวทย์มนต์ของเรานั้น ไม่สามารถเข้าไปด้วยหน้าจอคอมพิวเตอร์ หรือLaptopของท่านได้</p>
+			<p class="indent-6 mt-2">ทางเราจึงขอแนะนำให้ท่าน เข้าสู่ประตูของเราด้วยอุปกรณ์ดังต่อไปนี้ :</p>
+			<ul class="ml-14 list-disc">
+				<li>Smartphone</li>
+				<li>Tablet</li>
+			</ul>
+		</div>
+	</div>
 </div>
 
 <div class="bg-gradient-to-b from-primary to-black overflow-x-hidden relative md:hidden">
