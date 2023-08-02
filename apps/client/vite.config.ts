78f2fa91af75eb922de450fe7810c9a420023d40
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2023-08-02 14:32:26 +0700

    Feat: PWA

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,6 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			strategies: 'injectManifest',
+			srcDir: 'src',
+			filename: 'service-worker.ts',
+			/* other pwa options */
+		})
+	]
 });
