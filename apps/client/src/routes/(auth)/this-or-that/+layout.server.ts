fed18f0de91e6ebebfe35cec3f8cc7ef3d255b4b
DrowningToast <supratouchgussii@gmail.com>
2023-08-03 19:57:40 +0700

    fix: redirect

--- apps/client/src/routes/(auth)/this-or-that/+layout.server.ts
+++ apps/client/src/routes/(auth)/this-or-that/+layout.server.ts
@@ -9,6 +9,16 @@ export const load: LayoutServerLoad = async ({ locals }) => {
 		throw redirect(307, '/home');
 	}
 
+	// if the user is sophomore and haven't entered the hints, redirect them to the hints first
+	if (user?.type === 'SOPHOMORE' && !user.sophomoreDetails?.hintsReady) {
+		throw redirect(307, '/set-hints');
+	}
+
+	// if the user is freshmen and haven't registered, redirect.
+	if (user?.type === 'FRESHMEN' && !user.freshmenDetails) {
+		throw redirect(307, '/regis');
+	}
+
 	return {
 		session
 	};
