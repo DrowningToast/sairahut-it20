9495aeff5d7bf4111a512b30cc0fbd9235384cb2
aaapwn <armmykung2547@gmail.com>
2023-08-03 22:01:23 +0700

    add title

--- apps/client/src/routes/(auth)/+layout.svelte
+++ apps/client/src/routes/(auth)/+layout.svelte
@@ -5,14 +5,17 @@
 
 <div class="bg-gradient-to-b from-primary to-black hidden md:block min-h-screen relative">
 	<div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-accent-alt border border-accent p-5 rounded-xl">
-		<h1 class=" font-Pridi text-2xl font-medium">เรียน : ท่านผู้เดินทางไปยังโลกเวทย์มนต์</h1>
+		<h1 class="text-center font-Pridi text-3xl font-medium mb-5">ประกาศจากกระทรวงเวทมนตร์</h1>
+		<h1 class=" font-Pridi text-xl font-normal">เรียน : ผู้มาเยือนโลกแห่งเวทมนตร์ทุกท่าน</h1>
 		<div class="ml-3 mt-3 font-light">
-			<p class="indent-6">เนื่องด้วยประตูสู่โลกเวทย์มนต์ของเรานั้น ไม่สามารถเข้าไปด้วยหน้าจอคอมพิวเตอร์ หรือLaptopของท่านได้</p>
-			<p class="indent-6 mt-2">ทางเราจึงขอแนะนำให้ท่าน เข้าสู่ประตูของเราด้วยอุปกรณ์ดังต่อไปนี้ :</p>
-			<ul class="ml-14 list-disc">
-				<li>Smartphone</li>
+			<p class="indent-6">เนื่องจากการจุติของจอมมาร ทำให้ผู้ที่มาเยือนโลกใบนี้ด้วยอุปกรณ์เวทมนตร์ชนิดดังต่อไปนี้ไม่สามารถเข้าสู่โลกเวทมนตร์ได้</p>
+			<ul class="list-disc ml-10">
+				<li>คอมพิวเตอร์</li>
+				<li>Laptop</li>
 				<li>Tablet</li>
+				<li>อุปกรณ์อื่นๆที่มีจอแสดงผลขนาดใหญ่</li>
 			</ul>
+			<p class="indent-6 mt-2">ทางสภาเวทมนต์จึงขอแนะนำให้ผู้มาเยือนทุกท่าน มาเยือนโลกใบนี้อีกครั้งด้วยอุปกรณ์เวทมนต์ชนิดโทรศัพท์พกพา</p>
 		</div>
 	</div>
 </div>
