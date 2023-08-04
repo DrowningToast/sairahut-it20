c8fd22a940313ebb14d1c2c0bec8770f49059ac8
DrowningToast <supratouchgussii@gmail.com>
2023-08-04 16:37:23 +0700

    fix: login button

--- apps/client/src/lib/server/routes/authRouter.ts
+++ apps/client/src/lib/server/routes/authRouter.ts
@@ -2,8 +2,10 @@ import { createRouter } from '../context';
 import { protectedProcedure, publicProcedure } from '../procedure';
 
 export const authRouter = createRouter({
-	getUser: publicProcedure.query(async ({ ctx }) => {
+	getUser: protectedProcedure.query(async ({ ctx }) => {
 		const user = { ctx };
+
+		console.log(user);
 
 		return user;
 	})
