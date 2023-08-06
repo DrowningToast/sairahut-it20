81ce9830c32313cafe9dcbe7a28ff4ba1c6f48e2
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2023-08-07 01:35:20 +0700

    Feat: 512x512 icon

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -18,6 +18,11 @@ export default defineConfig({
 						"src": "icons/icon-192.png",
 						"sizes": "192x192",
 						"type": "image/png"
+					},
+					{
+						"src": "icons/icon-512.png",
+						"sizes": "512x512",
+						"type": "image/png"
 					}
 				],
 				"theme_color": "#1E3A8A",
