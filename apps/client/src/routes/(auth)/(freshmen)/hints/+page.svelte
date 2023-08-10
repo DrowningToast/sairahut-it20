f14d12adf7710aea29675b3d9047ca62939ac50c
aaapwn <armmykung2547@gmail.com>
2023-08-11 01:11:31 +0700

    add fucking localStorage

--- apps/client/src/routes/(auth)/(freshmen)/hints/+page.svelte
+++ apps/client/src/routes/(auth)/(freshmen)/hints/+page.svelte
@@ -1,34 +1,27 @@
 <script lang="ts">
 	const hints = [
-		{ hint: 'Text', activate: true },
-		{ hint: 'Text', activate: true },
-		{ hint: 'Text', activate: true },
-		{ hint: 'Text', activate: false },
-		{ hint: 'Text', activate: false },
-		{ hint: 'Text', activate: false },
-		{ hint: 'Text', activate: false },
-		{ hint: 'Text', activate: false }
+		'text', 'text', 'text'
 	];
 </script>
-
-<div class="font-krub text-white drop-shadow-[0px_0px_10px_#FFAEBD]">
-	<h1 class="text-2xl font-bold">คำใบ้จากเหล่าภูต</h1>
-	<p class="text-base">คำใบ้พวกนี้ จะนำพาเราไปเจอใครกันนะ</p>
+<div class="mb-36">
+	<div class="font-krub text-white drop-shadow-[0px_0px_10px_#FFAEBD]">
+		<h1 class="text-2xl font-bold">คำใบ้จากเหล่าภูต</h1>
+		<p class="text-base">คำใบ้พวกนี้ จะนำพาเราไปเจอใครกันนะ</p>
+	</div>
+	<p class="text-white font-krub text-6xl font-bold text-center my-8 drop-shadow-[0px_0px_13px_#FFAEBD]">{hints.length}/10</p>
+	<div class="flex flex-col gap-y-4 mt-3">
+		{#each hints as hint}
+				<div
+					class="bg-[#F58CB7]/25 rounded-2xl w-full py-2 px-3 shadow-[0_4px_4px_0px_rgba(0,0,0,0.25)]"
+				>
+					<p class="text-base text-white font-krub">{hint}</p>
+				</div>
+	
+		{/each}
+	</div>
 </div>
-<div class="flex flex-col gap-y-4 mt-3">
-	{#each hints as hint}
-		{#if hint.activate}
-			<div
-				class="bg-[#F58CB7]/25 rounded-2xl w-full py-2 px-3 shadow-[0_4px_4px_0px_rgba(0,0,0,0.25)]"
-			>
-				<p class="text-base text-white font-krub">{hint.hint}</p>
-			</div>
-		{:else}
-			<div
-				class="bg-[#4E1E31]/50 rounded-2xl w-full py-2 px-3 shadow-[0_4px_4px_0px_rgba(0,0,0,0.25)]"
-			>
-				<p class="text-base text-white font-krub">... (ตรงนี้ยังไม่เปิดคำใบ้)</p>
-			</div>
-		{/if}
-	{/each}
+<div class="text-yellow-100 text-center px-20 font-Pridi text-xs font-thin">
+	<p>คำอธิบาย</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio explicabo blanditiis animi placeat natus exercitationem iusto debitis, eligendi quo ea, dicta aspernatur commodi nisi consequuntur fugit dolorum praesentium. Facilis, deserunt.</p>
+	<p class="text-yellow-200 mt-10">อ่านคำใบ้แล้วตามหาภูติตัวเอง</p>
 </div>
--- apps/client/src/routes/(auth)/home/+page.svelte
+++ apps/client/src/routes/(auth)/home/+page.svelte
@@ -15,6 +15,7 @@
 	import AlertDialogAction from '$components/ui/alert-dialog/AlertDialogAction.svelte';
 	import { browser } from '$app/environment';
 	import { View } from 'lucide-svelte';
+	import { onMount } from 'svelte';
 	const { session, homePageState, user, playerType } = $page.data as PageData;
 
 	let isQrCodeActive = new Date() >= homePageState.qrCode.activateDate;
@@ -27,7 +28,10 @@
 		'family' : 'Fairy'
 	}
 
-	$: isOpen = false;
+	let isOpen:boolean = false;
+	onMount(() => {isOpen = localStorage.getItem('isOpen') === 'true';});
+	// isOpen = localStorage.getItem('isOpen') === 'true';
+	console.log(isOpen);
 </script>
 
 <div>
@@ -64,7 +68,7 @@
 				</div>
 				<div class={!isOpen ? 'block' : 'hidden'}>
 					<div class="flex flex-col items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-						<button on:click={() => {isOpen = true;}}><View size={72} color="#AF9E6E" strokeWidth={1} /></button>
+						<button on:click={() => {localStorage.setItem('isOpen', 'true'); isOpen = true;}}><View size={72} color="#AF9E6E" strokeWidth={1} /></button>
 						<p class="text-yellow-100 drop-shadow-[0px_0px_4px_#FFD130] text-xs">ดูภูติของท่าน</p>
 					</div>
 				</div>
